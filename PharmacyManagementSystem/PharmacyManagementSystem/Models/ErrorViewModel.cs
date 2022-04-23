8c1338a78f19b12bebba489ce2c6e55df213e036
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-04-23 18:03:56 +0200

    init proj

---
+++ PharmacyManagementSystem/PharmacyManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PharmacyManagementSystem.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
